b0bf7181a64dd40064af458a986119e06b524946
MarioQuiroga <marioquiro17@gmail.com>
2018-05-29 21:36:06 -0300

    update

--- public/js/buscarInsumoProducto.js
+++ public/js/buscarInsumoProducto.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$("#btnBuscar").click(function() {
+	$("#btnBuscar").on("click", function() {
+		$("#tbodyResultados").remove();
 		console.log($("#codigo").val());
 		console.log($("#nombre").val());
 		console.log($("#categoria").val());
@@ -19,36 +20,24 @@ $(document).ready(function() {
 	  		
 	  	}).done(
 	  		function(data) {	  				
-					console.log(data);
-				    var tbody =  $("#tbodyResultados");
-				    //tbody.empty();
-				    data.forEach(function(item, index){
-				    	var tr=document.createElement("tr");
-			        	tr.setAttribute("id","tr"+index);
-			        	//var i = 
-			        	$.each(item, function(i,item1){
-			        		var td=document.createElement("td");	    			
-				    		td.innerHTML = item[i];
-				    		tr.appendChild(td);        			
+				console.log(data);
+			    var tbody =  $("#tbodyResultados");
+			    //tbody.empty();
+			    data.forEach(function(item, index){
+			    	var tr=document.createElement("tr");
+		        	tr.setAttribute("id","tr"+index);
+		        	
+		        	$.each(item, function(i,item1){
+		        		var td=document.createElement("td");	    			
+			    		td.innerHTML = item[i];
+			    		tr.appendChild(td);        			
 
-			        	})
-			        	tbody.append(tr);
-				    });
-				    /*
-				    for (var i = 0; i < data.lenght; i++) {
-			    		var tr=document.createElement("tr");
-			        	tr.setAttribute("id","tr"+i);
-				    	for (var j = 0; j < data[i].lenght; j++) {
-				    		var td=document.createElement("td");	    			
-				    		td.innerHTML = data[i][j];
-				    		tr.appendChild(td);        			
-				    	}	
-				    	tbody.append(tr);
-				    }*/
-			}
-	  	).fail(function(){
-	  		console.log('Error');
-	  	}
+		        	})
+		        	tbody.append(tr);
+			    });				 
+			}).fail(function(){
+	  			console.log('Error');
+		  	}
 	  	);
 
 	/*	
