6debf0a942cd11aa9e22c9ead1db5ff4f69a8d3e
agrup <agustinrodriguez206@gmail.com>
2018-06-05 04:02:56 -0300

    agregar formulacion

--- public/js/administracion/agregarFormulacion.js
+++ public/js/administracion/agregarFormulacion.js
@@ -1,76 +1,68 @@
 $(document).ready(function() {
 
 
-
 	$('#trFormulacion').hide();
-
 	$('#agregarInsumo').on("click", function(){		
-
-
 		var row = $('#trFormulacion');
+		console.log(row);
 		var newRow=row.clone();
-		$(newRow).show();
-		alert(newRow);
-		newRow.on("change", function(){   //cambiar el tipo de Unidad dependiendo el producto
-
+		
 		$(newRow).show();
 		newRow.addClass('trFormulacion');
 		$('input', newRow).addClass('inputFormulacion');
+
 		$('select', newRow).addClass('selectFormulacion');
-
 		newRow.on("change", function(){   //cambiar el tipo de Unidad dependiendo el producto		
-
 			var parent = this.closest('tr');
-	
+			console.log(parent);		
 			var tu= $('option:selected', this).attr("data-unit");		
 			console.log(tu);
 			var i = parent
-			var label = $('td:last', parent);
-
+			var label = $('td span', parent);
+			console.log(label);
 			label.text(tu);
 		});		
+		
+
 		newRow.insertAfter(row);
+		$('.eliminarRow').on('click', function(){
+			var parent = this.closest('tr');
+			parent.remove();
+		});
 		var cantidad = row.find("input#cantidad");
 		var tu = row.find("span#tdTipoUnidad");
 		tu.attr({id:cantidad});
-
+		console.log(cantidad);
 		cantidad.removeAttr('placeholder');
 
 	});
 
-
-
 	$('.selectInsumo').on("change", function(){   //cambiar el tipo de Unidad dependiendo el producto
 		
 		var parent = this.closest('tr');
-		
+		console.log(parent);		
 		var tu= $('option:selected', this).attr("data-unit");		
-
-		var i = parent
-		var label = $('td:last', parent);
-
+		console.log(tu);
+		//var i = parent;
+		var label = $('td span', parent);
+		console.log(label);
 		label.text(tu);
 	});
 
 	$('#guardarFormulacion').on("click", function(){
-		//var dataFormulacion[];
-
-	});
-
-
-
-
-	/*
-	$('#guardarFormulacion').on("click", function(){
+		event.preventDefault();	
 		var dataFormulacion = [];
-		var b = true;	
+		var b = true;
+		
 		var inputs = $('.inputFormulacion');
+		
 		inputs.each(function(){			
 			if ($(this).val()=="") {
 				b = false;			
 			}
 		});
 		var selects = $('.selectFormulacion');
+		
 		selects.each(function(){
 			console.log($('option:selected', this).val());
 			if($('option:selected', this).val()==null){
@@ -78,6 +70,8 @@ $(document).ready(function() {
 			}
 			
 		});
+		
+
 		if(b){
 			$('.trFormulacion').each(function(index){
 				
@@ -85,17 +79,22 @@ $(document).ready(function() {
 				var id = $('option:selected', this).attr("data-id");
 				var cantidad = $('input#cantidad', this).val();
 				console.log(cantidad);
+				console.log('22222222222222')
 				console.log(id);			
 				var row = [id, cantidad];
 				dataFormulacion.push(row);
+				alert(row);
 			});
 			console.log(dataFormulacion);
 			$('#inputHidden').val(dataFormulacion);
-			//console.log(input.attr("name"));
+			
+			
 		}else{
 			alert("Por Favor complete el formulario")
 		}	
-	*/
+
+	});
+	
 
 	$('#myForm').on('submit', function(){
 		event.preventDefault();			
@@ -113,13 +112,10 @@ $(document).ready(function() {
 		this.submit();
 	});
 
-
+	$('.eliminarRow').on('click', function(){
+		var parent = this.closest('tr');
+		parent.remove();
 	});
-	/*$('#inputTipoUnidad').on("change", function(){
-		console.log(this);
-		var tu = this.val();
-		$('#labelTipoUnidad').text(tu);
-	});*/
 
 
 });
